92032b86f127d614f85f529c790b25d0a8fdde22
divy soni <chetanshudha267@gmail.com>
2024-07-07 23:59:25 +0530

    if else task some done

--- control statement/If_Else_Tasks.js
+++ control statement/If_Else_Tasks.js
@@ -124,13 +124,38 @@ else {
 // Test Data : 7 9
 // Expected Output :
 // The coordinate point (7,9) lies in the First quadrant.
+var X = +prompt("Enter X Coordinates : ");
+var Y = +prompt("Enter Y Coordinates : ");
 
-
+if(X > 0 && Y > 0) {
+    document.write(`Entered Coordinates (${X,Y}}) both are Positive so It's First Quadrant.`);
+}
+else if(X<0 && Y>0) {
+    document.write(`Entered Coordinates (${X,Y}}) Where X is Positive and Y is Negative so It's Second Quadrant.`);
+}
+else if(X<0 && Y<0) {
+    document.write(`Entered Coordinates (${X,Y}}) both are Negative so It's Third Quadrant.`);
+}
+else if(X>0 && Y<0) {
+    document.write(`Entered Coordinates (${X,Y}}) Where X is Positive and Y is Negative so It's Fourth Quadrant.`);
+}
+else {
+    document.write(`ERROR : Invalid Input, Please Try Again.`);
+}
 // 10. Write a C program to determine eligibility for admission to a professional course based on the following criteria: Go to the editor
 // Eligibility Criteria : Marks in Maths >=65 and Marks in Phy >=55 and Marks in Chem>=50 and Total in all three subject >=190 or Total in Maths and Physics >=140 ------------------------------------- Input the marks obtained in Physics :65 Input the marks obtained in Chemistry :51 Input the marks obtained in Mathematics :72 Total marks of Maths, Physics and Chemistry : 188 Total marks of Maths and Physics : 137 The candidate is not eligible.
 // Expected Output :
 // The candidate is not eligible for admission.
+var maths = +prompt("Enter Your Maths Marks :");
+var phy = +prompt("Enter Your Physics Marks :");
+var chem = +prompt("Enter Your Chemistry Marks :");
 
+if(maths>=65 && phy>=55 && chem>=50) {
+    document.write(`The Candidate is eligible for admission.`);
+}
+else {
+    document.write(`The Candidate is not eligible for admission.`);
+}
 
 // 11. Write a C program to calculate the root of a quadratic equation.
 // Test Data : 1 5 7
