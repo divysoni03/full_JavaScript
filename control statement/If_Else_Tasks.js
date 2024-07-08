7b5aba520467102e558dd5ed6e78074062c42d17
divy soni <chetanshudha267@gmail.com>
2024-07-08 23:45:06 +0530

    tasks 18

--- control statement/If_Else_Tasks.js
+++ control statement/If_Else_Tasks.js
@@ -281,7 +281,18 @@ else {
 // 500 700
 // Expected Output :
 // You can booked your profit amount : 200
+var send = +prompt("Enter Sending amount :");
+var receive = +prompt("Enter Receiving amount :");
 
+var total = receive-send;
+
+console.log(`Money Sent : ${send},\nMoney Received : ${receive},\n`);
+if(total <0) {
+    console.log(`Total Loss :${total}`);
+}
+else {
+    console.log(`Total Profit :${total}`);
+}
 
 // 19. Write a program in C to calculate and print the electricity bill of a given customer. The customer ID, name, and unit consumed by the user should be captured from the keyboard to display the total amount to be paid to the customer.
 
