89c2c48fa5ac6a52666158c7c06a136f942b5be5
divy soni <chetanshudha267@gmail.com>
2024-07-22 23:04:11 +0530

    added some explanation

--- Objects/obj-secondface.js
+++ Objects/obj-secondface.js
@@ -24,7 +24,8 @@
 //     title: 'The Road Ahead',
 //     readingStatus: true
 // }
-// console.log({...library});
+// console.log({...library}); // in first we are converting obj into array using spread operator and after printing
+// we have to convert it into obj to resolve further issues.
 
 
 //destructuring
@@ -53,22 +54,30 @@
 // console.log(newObj.address);
 
 // var {address:{city}} = myObj
-// console.log(city);
+// console.log(city); // prints GNR.
+
+// var {address:{city},address : {state}, address : {country}} = myObj
+// console.log(city, state, country); // we can access all using this syntax
+
+// var {address : add} = myObj; // we can also do this
+// console.log(add.city, add.country, add.state);
+
+
+
 
 //Computed Props
-var key1 = "fname"
-var key2 = "lname"
-var key3 = "age"
-var key4 = "gender"
-var key5 = "address"
+// var key1 = "fname"
+// var key2 = "lname"
+// var key3 = "age"
+// var key4 = "gender"
+// var key5 = "address"
 
+// var person = {
+//     [key1]: "Ronak",
+//     [key2]: "Patel",
+//     [key3]: 23,
+//     [key4]: "Male",
+//     [key5]: "IN",
+// }
 
-var person = {
-    [key1]: "Ronak",
-    [key2]: "Patel",
-    [key3]: 23,
-    [key4]: "Male",
-    [key5]: "IN",
-}
-
-console.log(person);
+// console.log(person);
