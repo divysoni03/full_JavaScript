4a4001e8294106d47fa43006cef9cd195ae7da8c
divy soni <chetanshudha267@gmail.com>
2024-07-23 18:12:26 +0530

    done without split methods

--- Functions/Task6.js
+++ Functions/Task6.js
@@ -1,9 +1,26 @@
 // -> Problem 6: Reverse a String
 // Statement: Write a named function 'reverseString' that takes a string as an argument and returns the string reversed. Also, write an expression function 'reverseStringExpression' that does the same.
 
+// function reverseString(str) {
+    
+//     let arr = str.split('');
+//     let i = 0,j = arr.length -1;
+//     while(i < j) {
+//         let ch = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = ch;
+//         i++;j--;
+//     }
+//     return arr.join('');
+// }
+
+// console.log(reverseString("1234"));
 function reverseString(str) {
     
-    let arr = str.split('');
+    let arr = [str.length];
+    for(const i in str) {
+        arr[i] = str[i];
+    }
     let i = 0,j = arr.length -1;
     while(i < j) {
         let ch = arr[i];
@@ -11,7 +28,11 @@ function reverseString(str) {
         arr[j] = ch;
         i++;j--;
     }
-    return arr.join('');
+    let str2 = "";
+    for (const i in arr) {
+        str2 += arr[i];
+    } 
+    return str2;
 }
 
 console.log(reverseString("1234"));
