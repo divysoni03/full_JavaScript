ac7ab74446f94a9c390696e7d40f98f87eaff356
divy soni <chetanshudha267@gmail.com>
2024-08-31 21:43:56 +0530

    use prototype, new keyword

--- OOPs/prototype.js
+++ OOPs/prototype.js
@@ -1,30 +1,3 @@
-// var obj = {
-//     key1: "Value1",
-//     key2: "Value2"
-// };
-
-// console.log(obj.__proto__);
-
-// var obj2 = Object.create(obj);
-// obj2.key3 = "Value3";
-
-// console.log(obj);
-// console.log(obj2);
-
-// console.log(obj.key1);
-// console.log(obj.key2);
-
-// console.log(obj2.key1);
-// console.log(obj2.key2);
-// console.log(obj2.key3);
-
-// console.log(obj.__proto__);
-// console.log(obj2.__proto__);
-
-// [[prototype]] === __proto__
-// prototype !== [[prototype]]
-
-
 /* 
 `__proto__`: A property that references the prototype of the object, from which it inherits methods and properties.
 
@@ -32,11 +5,11 @@
 
 */
 
-const parent = { greet() { console.log('Hello!'); } };
-const child = Object.create(parent);    //true
+// const parent = { greet() { console.log('Hello!'); } };
+// const child = Object.create(parent);    //true
 
-console.log(child.__proto__ === parent);
-child.greet();
+// console.log(child.__proto__ === parent);
+// child.greet();
 
 /*
 const grandParents = { wisdom: 'Ancient Knowledge', recipe: 'Traditional Dish' };
@@ -52,3 +25,36 @@ console.log(child.__proto__.__proto__ === grandParents);
 console.log(child.wisdom);
 console.log(child.newSkills);
 */
+
+// -------------------------------------------------------------------------
+// Use Prototype
+
+// function Person(name) {
+//     this.name = name;
+// }
+
+// Person.prototype.greet = function() {
+//     console.log(`Hello, my name is ${this.name}`);
+// }
+
+// const person1 = new Person('Tom');
+// const person2 = new Person('Jerry');
+
+// person1.greet();
+// person2.greet();
+// -------------------------------------------------------------------------
+// new Keyword
+
+// `new` keyword does the following 4 things:
+// 1. Creates a new object.
+// 2. Sets the prototype of the new object to the prototype of the constructor function.
+// 3. Binds this inside the constructor function to the new object.
+// 4. Returns the new object, unless the constructor function returns its own object.
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+const person1 = new Person('Annie', 21);
+console.log(person1);
