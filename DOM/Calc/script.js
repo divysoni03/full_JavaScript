e4a1925880ccd8951e90d88aac62068509b9cd16
divy soni <chetanshudha267@gmail.com>
2024-11-23 12:10:13 +0530

    calc done

--- DOM/Calc/script.js
+++ DOM/Calc/script.js
@@ -1,55 +1,127 @@
-let input = document.querySelector("input");
-let buttons = document.querySelector(".container");
-let allOP = ['/','*','+','-','%'];
+var div = document.querySelector('div');
+var input = document.querySelector('input');
+var AllOp = ['+',"-","*","/","%"];
+var count=0;
+var flag = false;
+var flag2 = true;
+var temp1 = '';
+var temp2 = '';
+var temp3 = '';
+div.addEventListener('click',function (event){
+var op = '';
+var outerTag = document.querySelector('.outer');
 
-
-buttons.addEventListener("click", (event) => {
-    if(event.target.tagName == "BUTTON") {
-        if(event.target.innerHTML == "AC") {
-            input.value = "";
-        }
-        else if(event.target.innerHTML == "DEL") {
-            // let arr = input.value.split('');
-            // arr.pop();
-            // input.value = arr.join('');
-
-            input.value = input.value.substring(0, input.value.length - 1);
-        }
-        else if(event.target.innerHTML == "=") {
-            let num1 = '';
-            let num2 = '';
-            let op = '';
-
-            for(let i=0; i < input.value.split('').length ; i++) {
-
-                if( allOP.includes(input.value.split('')[i] )) {
-                    op = input.value.split('')[i];
-                    num2 = input.value.split('').slice(i+1).join('');
-                    break;
+    if(event.target.tagName=="BUTTON"){
+        if(event.target.innerHTML!='AC' && event.target.innerHTML !='DEL' && event.target.innerHTML!="="){
+                if(flag){
+                    input.value = '';
+                    flag = false;
                 }
-                num1+=input.value.split('')[i]; 
+                if(AllOp.includes(event.target.innerHTML)){
+                    if(outerTag.innerHTML==''){
+                        outerTag.innerHTML = String(Number(outerTag.innerHTML)+ Number(input.value))+event.target.innerHTML;
+                    }
+                    else{
+                        outerTag.innerHTML = String(Number(outerTag.innerText.slice(0,outerTag.innerText.length-1))+ Number(input.value))+event.target.innerHTML;
+                    }
+                    input.value =  outerTag.innerText.slice(0,outerTag.innerText.length-1);
+                    flag = true;
+                    return;
+                }
+                if(AllOp.includes(event.target.innerHTML) && input.value[input.value.length-1]=='.'){
+                    input.value = input.value.slice(0,input.value.length-1)+event.target.innerHTML;
+                    return;
+                }
+                if(AllOp.includes(input.value[input.value.length-1])){
+                    op = input.value[input.value.length-1];
+                    count++;
+                    if(event.target.innerHTML=='.'){
+                        input.value =input.value+'0'+event.target.innerHTML;
+                        return;
+                    }
+                }
+                else if( count && input.value.split('').filter((a)=>{return a=='.'}).length==1){
+                    input.value +=event.target.innerHTML;
+                    return;
+                }
+            if(input.value[0]=='0'){
+                if(event.target.innerHTML=='00'){
+                    return;
+                }else if(event.target.innerHTML=='.'){
+                    if(op=='' && input.value.split('').includes('.')){
+                        return;
+                    }
+                    else{
+                        input.value +=event.target.innerHTML;
+                        return;    
+                    }
+                }
             }
-
-            switch(op){
-                case '+' : 
-                    input.value = (+num1)+(+num2);
-                    break;
-                case '-' :
-                    input.value = (+num1)-(+num2);
-                    break;
-                case '*' :
-                    input.value = (+num1)*(+num2);
-                    break;
-                case '/' :
-                    input.value = (+num1)/(+num2);
-                    break;
-                case '%' :
-                    input.value = (+num1)%(+num2);
-                    break;
+            else if(event.target.innerHTML=='.'){
+                if(op=='' && input.value.split('').includes('.')){
+                    return;
+                }
+                else{
+                    input.value +=event.target.innerHTML;
+                    return;    
+                }
+            }
+            if(input.value[0]=='0' && input.value.length==1){
+                input.value =event.target.innerHTML;
+            }else{
+                input.value +=event.target.innerHTML;
+            }
+        }else if(event.target.innerHTML=='='){
+            if(flag2==true){
+                temp3 = input.value;
+                for (let i = 0; i < outerTag.innerText.split('').length; i++) {
+                    if(AllOp.includes(outerTag.innerText.split('')[i])){
+                        var op = outerTag.innerText.split('')[i];
+                        temp2 = input.value;
+                        break;
+                    }
+                    temp1+=outerTag.innerText.split('')[i]; 
+                }
+                outerTag.innerText +=input.value + "=";
+                switch(op){
+                    case '+' : input.value = (+temp1)+(+temp2);
+                                break;
+                }
+                flag2 = false;
+            }
+            else if(flag2==false){
+                outerTag.innerText =input.value+outerTag.innerText.slice(outerTag.innerText.length-2-temp3.length);
+                input.value=(+input.value) +(+temp3);
             }
         }
-        else {
-            input.value += event.target.innerHTML;
+        else if(event.target.innerHTML=='AC'){
+            input.value = "0";
+            outerTag.innerHTML = "";
+        }
+        else if(event.target.innerHTML=='DEL'){
+
+            input.value = input.value.slice(0,input.value.length-1);
+            if(input.value.length==0){
+                input.value = "0";
+            }
         }
     }
 });
+
+
+// "Zafar".slice(0,4)
+//  0123 
+
+
+// console.log(Number(document.querySelector('section').innerText));
+// div.onclick = function (){
+//     console.log("we");
+// }
+// div.onclick = function (){
+//     console.log("New");
+// }
+// console.log(document.querySelector('button').tagName);
+
+// div.addEventListener(event,function);
+                    
+                      // console.log(input.value.split('').includes('+'));  //true 
