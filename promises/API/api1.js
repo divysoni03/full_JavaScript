91303736f985f4bd557ba6acd87d159fa5360be1
divy soni <chetanshudha267@gmail.com>
2024-12-21 11:58:46 +0530

    web history in api

--- promises/API/api1.js
+++ promises/API/api1.js
@@ -1,16 +1,32 @@
-const btn = document.querySelector('button');
+// --> NAVIGATION API AND GEO COORDINATES :
 
-btn.addEventListener('click',function(){
-    if(navigator.geolocation){
-        // myFunction();
-        navigator.geolocation.getCurrentPosition(myFunction);
-    }else{
-        console.log("wert");
-    }
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click',function(){
+//     if(navigator.geolocation){
+//         // myFunction();
+//         navigator.geolocation.getCurrentPosition(myFunction);
+//     }else{
+//         console.log("wert");
+//     }
+// });
+
+
+// function myFunction(position){
+//     console.log(position.coords.latitude);
+//     console.log(position.coords.longitude);
+// }
+
+
+// --> HISTORY WEB API - TO MOVE BETWEEN PAGES
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", ()=> {
+    console.log("going back");
+    // history.back(); // to go previous page
+
+    // history.forward(); // to go ahead next page
+
+    // history.go(number); // to go back and forward (-number for going back and positive number for going forward)
 });
-
-
-function myFunction(position){
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-}
